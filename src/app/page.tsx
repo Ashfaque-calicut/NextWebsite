cbf86c19894ec6db111998474964aef6b53a337c
Ashfaque-calicut <ashfaquepalazhi@gmail.com>
2023-09-17 12:54:49 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Hero from '@/components/Hero'
+import NewProducts from '@/components/NewProducts'
+import Testimonial from '@/components/TestiMoniel'
+
+export default function Home() {
+  return (
+    <main>
+      <Hero/>
+      <NewProducts/>
+      <Testimonial/>
+    </main>
+  )
+}
